26e1bb06ed9f7768387898dfa46e3e71cf20382e
Breno Amaral <bramaral@girolando.com.br>
2024-09-13 16:04:07 -0300

    Melhorando layout da pagina de usuarios

--- src/components/users/UserPage.vue
+++ src/components/users/UserPage.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold mb-6 text-gray-700">Usuários</h1>
+    <div class="bg-white rounded-lg shadow-md p-2">
+      <div class="flex justify-between items-center p-4 bg-gradient-to-br from-cyan-500 to-cyan-800 rounded">
+        <div>
+          <h1 class="text-3xl font-bold  text-white">Usuários</h1>
+        </div>
+        <div class="flex justify-end">
 
-    <div class="bg-white rounded-lg shadow-md p-4">
-      <div class="flex justify-end mb-4">
-        <!-- Botão de adicionar usuário -->
-        <button
-            @click="openModal()"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
-          </svg>
-
-        </button>
+          <!-- Botão de adicionar usuário -->
+          <button
+              @click="openModal()"
+              class="px-1 py-2 bg-white text-gray-950 hover:bg-gray-100 rounded-full"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 ml-2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+            </svg>
+          </button>
+        </div>
       </div>
 
       <!-- Data Table -->
       <table class="min-w-full bg-white rounded-lg">
-        <thead class="bg-gray-100">
+        <thead class="border-b border-gray-300">
         <tr>
           <th class="py-2 px-4 text-left text-gray-600">Nome</th>
           <th class="py-2 px-4 text-left text-gray-600">Email</th>
@@ -25,29 +29,20 @@
         </tr>
         </thead>
         <tbody>
-        <tr
-            v-for="user in users"
-            :key="user.id"
-            class="border-b hover:bg-gray-50"
-        >
-          <td class="py-2 px-4 text-gray-700">{{ user.name }}</td>
-          <td class="py-2 px-4 text-gray-700">{{ user.email }}</td>
-          <td class="py-2 px-4">
+        <tr v-for="user in users" :key="user.id" class="hover:bg-gray-200">
+          <td class="py-2 px-4 text-gray-700 border-b border-gray-300">{{ user.name }}</td>
+          <td class="py-2 px-4 text-gray-700 border-b border-gray-300">{{ user.email }}</td>
+          <td class="py-2 px-4 border-b border-gray-300">
             <div class="flex space-x-2">
               <!-- Botão de editar -->
-              <button
-                  @click="editUser(user)"
-              >
+              <button @click="editUser(user)">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                 </svg>
               </button>
               <!-- Botão de deletar -->
-              <button
-                  @click="deleteUser(user.id)"
-
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <button @click="deleteUser(user.id)">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="size-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
               </button>
@@ -56,6 +51,8 @@
         </tr>
         </tbody>
       </table>
+
+
     </div>
 
     <!-- Modal de edição/adicionar usuário -->
